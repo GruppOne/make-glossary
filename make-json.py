00d808e0b46a74d31d99091489aa3dc1a193a27e
erclu <30255227+erclu@users.noreply.github.com>
2020-01-12 22:57:49 +0100

    feat: capitalize the first letter of entries
    includes a bugfix. Entries with characters not allowed as dict keys
    are now handled correctly

--- make-json.py
+++ make-json.py
@@ -6,9 +6,8 @@ import argparse
 argumentParser = argparse.ArgumentParser()
 
 argumentParser.add_argument(
-    "-v", "--version",
-    help="sow program version and exit",
-    action="store_true")
+    "-v", "--version", help="show program version and exit", action="store_true"
+)
 
 args = argumentParser.parse_args()
 
@@ -16,31 +15,49 @@ if args.version:
     print("make-json.py version 1.0.1\nLicensed under the GPLv3.0")
     exit()
 
-jsonFilePath = Path('.', 'commons', 'glossario.json')
+jsonFilePath = Path(".", "commons", "glossario.json")
 
 if not jsonFilePath.exists():
     jsonFilePath.touch()
-    jsonFile = jsonFilePath.open('a', encoding='utf-8')
-    jsonFile.write('{\n')
-    for letter in [chr(i) for i in range(ord('A'), ord('Z') + 1)]:
+    jsonFile = jsonFilePath.open("a", encoding="utf-8")
+    jsonFile.write("{\n")
+    for letter in [chr(i) for i in range(ord("A"), ord("Z") + 1)]:
         jsonFile.write(f'\t"{letter}": ' + "{ }")
-        if letter != 'Z':
-            jsonFile.write(',')
-        jsonFile.write('\n')
-    jsonFile.write('}\n')
+        if letter != "Z":
+            jsonFile.write(",")
+        jsonFile.write("\n")
+    jsonFile.write("}\n")
     jsonFile.close()
 
-with jsonFilePath.open('r', encoding='utf-8') as jsonFile:
+with jsonFilePath.open("r", encoding="utf-8") as jsonFile:
     glossary = json.load(jsonFile)
 
-for file in Path('.').glob('**/*.tex'):
-    with file.open('r', encoding='utf-8') as openedFile:
-        for line in openedFile:
-            for match in re.finditer(r'\\glossario\{.*?\}', line):
-                entry = match.group()[11:-1]
+for file in Path(".").glob("**/*.tex"):
+    with file.open("r", encoding="utf-8") as openedFile:
+        for lineNumber, line in enumerate(openedFile):
+            for match in re.finditer(r"\\glossario\{(?P<entry>.*?)\}", line):
+                entry = match.group("entry")
                 initial = entry[0].upper()
-                if entry not in glossary[initial]:
-                    glossary[initial][entry] = ''
 
-with jsonFilePath.open('w', encoding='utf-8') as jsonFile:
+                if len(entry) < 2:
+                    print(f"skipping entry {entry}, too short")
+                    continue
+
+                # cannot use the str.capitalize() function because it lowers every
+                # other letter
+                capitalizedEntry = initial + entry[1:]
+
+                try:
+                    if capitalizedEntry not in glossary[initial]:
+                        glossary[initial][capitalizedEntry] = ""
+
+                except KeyError:
+                    print("This entry is not a suitable dictionary key.")
+                    print(f"File: {file}")
+                    print(f"Line number: {lineNumber}")
+                    print(f"Line: {line}")
+                    print(f"Entry: {capitalizedEntry}")
+
+
+with jsonFilePath.open("w", encoding="utf-8") as jsonFile:
     json.dump(glossary, jsonFile, indent=4, sort_keys=True)
